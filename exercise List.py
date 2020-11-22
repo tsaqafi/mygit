f2f1d18be5de93268eba5e0b10bc046e2be4e752
tsaqafi <73459739+tsaqafi@users.noreply.github.com>
2020-11-22 11:16:37 +0800

    Exercise List

---
+++ exercise List.py
@@ -0,0 +1,9 @@
+# List less than ten
+# list number elements if conditional
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for i in a:  # for iterate each element in the list
+    if i < 5:  # Conditional statement if number is less than 5 is printed
+        print(i)
+
